76e83064195536df1516073a2095c8cc5363d024
DuckManZach <zacharyraytoma@proton.me>
2025-04-16 20:12:23 -0600

    Got em showing up

---
+++ Content.Server/Roles/PirateRoleComponent.cs
@@ -0,0 +1,11 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Roles;
+
+/// <summary>
+///     Added to mind role entities to tag that they are a space dragon.
+/// </summary>
+[RegisterComponent]
+public sealed partial class PirateRoleComponent : BaseMindRoleComponent
+{
+}
