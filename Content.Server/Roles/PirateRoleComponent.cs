ca65c5a3a24db638d4b204c4cf1f0f0721f662e4
DuckManZach <zacharyraytoma@proton.me>
2025-04-19 19:02:51 -0600

    Added comments

--- Content.Server/Roles/PirateRoleComponent.cs
+++ Content.Server/Roles/PirateRoleComponent.cs
@@ -3,7 +3,7 @@ using Content.Shared.Roles;
 namespace Content.Server.Roles;
 
 /// <summary>
-///     Added to mind role entities to tag that they are a space dragon.
+///     Added to mind role entities to tag that they are a Pirate.
 /// </summary>
 [RegisterComponent]
 public sealed partial class PirateRoleComponent : BaseMindRoleComponent
