91ca09be3c49a58d27f02e38a47dd93cb75b486c
dffdff2423 <dffdff2423@gmail.com>
2025-04-28 14:14:55 -0500

    Alt Title Icons

--- Content.Server/_CD/Loadouts/RenameIdSystem.cs
+++ Content.Server/_CD/Loadouts/RenameIdSystem.cs
@@ -1,10 +1,12 @@
 ﻿using Content.Server.Access.Systems;
 using Content.Server.GameTicking;
+using Content.Server.StationRecords.Systems;
 using Content.Shared.Access.Components;
 using Content.Shared.Access.Systems;
 using Content.Shared.GameTicking;
 using Content.Shared.Inventory;
 using Content.Shared.PDA;
+using Content.Shared.StationRecords;
 
 namespace Content.Server._CD.Loadouts;
 
@@ -12,6 +14,7 @@ public sealed class RenameIdSystem : EntitySystem
 {
     [Dependency] private readonly InventorySystem _inventorySystem = default!;
     [Dependency] private readonly SharedIdCardSystem _idCardSystem = default!;
+    [Dependency] private readonly StationRecordsSystem _records = default!;
 
     public override void Initialize()
     {
@@ -20,7 +23,7 @@ public sealed class RenameIdSystem : EntitySystem
         // We need to subscribe to both of these because RulePlayerJobsAssignedEvent only fires on round start and
         // messes up what we do in PlayerSpawnCompleteEvent
         SubscribeLocalEvent<RulePlayerJobsAssignedEvent>(OnJobsAssigned, after: [ typeof(PresetIdCardSystem) ]);
-        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawn);
+        SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawn, after: [ typeof(StationRecordsSystem) ]);
     }
 
     private void OnJobsAssigned(RulePlayerJobsAssignedEvent args)
@@ -28,11 +31,16 @@ public sealed class RenameIdSystem : EntitySystem
         var query = EntityQuery<RenameIdComponent, PdaComponent>();
         foreach (var (rename, pda) in query)
         {
-            if (pda.ContainedId is { } id
-                && TryComp<IdCardComponent>(id, out var card))
+            if (pda.ContainedId is not { } id ||
+                !TryComp<IdCardComponent>(id, out var card))
+                continue;
+
+            if (rename.NewIcon != null)
             {
-                _idCardSystem.TryChangeJobTitle(id, Loc.GetString(rename.Value), card);
+                card.JobIcon = rename.NewIcon; // TryChangeJobTitle dirties the ID for us
+                FlushIdIconToRecords(id, rename.NewIcon);
             }
+            _idCardSystem.TryChangeJobTitle(id, Loc.GetString(rename.Value), card);
         }
     }
 
@@ -42,12 +50,31 @@ public sealed class RenameIdSystem : EntitySystem
         if (!_inventorySystem.TryGetSlotEntity(player, "id", out var pdaUid))
             return;
 
-        if (TryComp<PdaComponent>(pdaUid, out var pda)
-            && TryComp<RenameIdComponent>(pdaUid, out var rename)
-            && pda.ContainedId is {} id
-            && TryComp<IdCardComponent>(id, out var card))
+        if (!TryComp<PdaComponent>(pdaUid, out var pda) ||
+            !TryComp<RenameIdComponent>(pdaUid, out var rename) ||
+            pda.ContainedId is not { } id ||
+            !TryComp<IdCardComponent>(id, out var card))
+            return;
+
+        if (rename.NewIcon != null)
         {
-            _idCardSystem.TryChangeJobTitle(id, Loc.GetString(rename.Value), card);
+            card.JobIcon = rename.NewIcon; // TryChangeJobTitle dirties the ID for us
+            FlushIdIconToRecords(id, rename.NewIcon);
         }
+        _idCardSystem.TryChangeJobTitle(id, Loc.GetString(rename.Value), card);
+    }
+
+    // Needed to update the icon on the manifest
+    // why do I need to do this
+    private void FlushIdIconToRecords(EntityUid targetId, string newJobIcon)
+    {
+        if (!TryComp<StationRecordKeyStorageComponent>(targetId, out var keyStorage) ||
+            keyStorage.Key is not { } key ||
+            !_records.TryGetRecord<GeneralStationRecord>(key, out var record))
+            return;
+
+        record.JobIcon = newJobIcon;
+
+        _records.Synchronize(key);
     }
 }
