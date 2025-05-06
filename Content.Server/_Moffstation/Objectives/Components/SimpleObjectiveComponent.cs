c37a2989e339cbf6366f4c69a471845bad73f6f9
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-05-06 02:55:35 -0600

    Updated name

--- Content.Server/_Moffstation/Objectives/Components/SimpleObjectiveComponent.cs
+++ Content.Server/_Moffstation/Objectives/Components/SimpleObjectiveComponent.cs
@@ -4,7 +4,7 @@ using Content.Server.Objectives.Systems;
 namespace Content.Server._Moffstation.Objectives.Components;
 
 [RegisterComponent]
-public sealed partial class StraightObjectiveComponent : Component
+public sealed partial class SimpleObjectiveComponent : Component
 {
     [DataField(required: true), ViewVariables(VVAccess.ReadWrite)]
     public string Title = string.Empty;
--- Content.Server/_Moffstation/Objectives/Systems/SimpleObjectiveSystem.cs
+++ Content.Server/_Moffstation/Objectives/Systems/SimpleObjectiveSystem.cs
@@ -5,18 +5,18 @@ namespace Content.Server._Moffstation.Objectives.Systems;
 /// <summary>
 /// This is a system/component that will take strings and plop them "straight" into the objective system, without conditions and the like. Good for freeform objectives that dont have varying targets.
 /// </summary>
-public sealed class StraightObjectiveSystem : EntitySystem
+public sealed class SimpleObjectiveSystem : EntitySystem
 {
     [Dependency] private readonly MetaDataSystem _metaData = default!;
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<StraightObjectiveComponent, ObjectiveGetProgressEvent>(OnGetProgress);
-        SubscribeLocalEvent<StraightObjectiveComponent, ObjectiveAfterAssignEvent>(OnAfterAssign);
+        SubscribeLocalEvent<SimpleObjectiveComponent, ObjectiveGetProgressEvent>(OnGetProgress);
+        SubscribeLocalEvent<SimpleObjectiveComponent, ObjectiveAfterAssignEvent>(OnAfterAssign);
     }
 
-    private void OnGetProgress(Entity<StraightObjectiveComponent> ent , ref ObjectiveGetProgressEvent args)
+    private void OnGetProgress(Entity<SimpleObjectiveComponent> ent , ref ObjectiveGetProgressEvent args)
     {
         args.Progress = GetProgress();
     }
@@ -26,7 +26,7 @@ public sealed class StraightObjectiveSystem : EntitySystem
         return 0f;
     }
 
-    private void OnAfterAssign(EntityUid uid, StraightObjectiveComponent comp, ref ObjectiveAfterAssignEvent args)
+    private void OnAfterAssign(EntityUid uid, SimpleObjectiveComponent comp, ref ObjectiveAfterAssignEvent args)
     {
         _metaData.SetEntityName(uid, GetTitle(comp.Title), args.Meta);
     }
