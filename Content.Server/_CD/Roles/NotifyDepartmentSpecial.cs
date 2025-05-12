35e247d23239f5151e10ef5e0fabdbe8d66f2bc3
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-05-11 23:22:18 -0600

    cleared up the stuff not being used

--- Content.Server/_CD/Roles/NotifyDepartmentSpecial.cs
+++ Content.Server/_CD/Roles/NotifyDepartmentSpecial.cs
@@ -11,15 +11,8 @@ namespace Content.Server._CD.Roles;
 public sealed partial class NotifyDepartmentSpecial : JobSpecial
 {
     [Dependency] private readonly ChatSystem _chatSystem = default!;
-    [DataField("notify_text", required: true)]
-    public string NotifyTextKey { get; private set; } = string.Empty;
-
-    [DataField("radio_channel", required: true, customTypeSerializer: typeof(PrototypeIdSerializer<RadioChannelPrototype>))]
-    public string RadioChannelKey { get; private set; } = string.Empty;
-
     public override void AfterEquip(EntityUid mob)
     {
-
         // Notify people on all stations.
         _chatSystem.DispatchStationAnnouncement(mob, Loc.GetString("prisoner-arrivals-notice"), colorOverride: Color.Orange);
     }
