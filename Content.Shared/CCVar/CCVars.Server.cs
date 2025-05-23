ae62c5049579f27f81d4730ea77c2b4f3c672611
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-05-22 19:54:14 -0600

    Added cvars and stuff

--- Content.Shared/CCVar/CCVars.Server.cs
+++ Content.Shared/CCVar/CCVars.Server.cs
@@ -65,4 +65,10 @@ public sealed partial class CCVars
     /// </summary>
     public static readonly CVarDef<bool> StartServerPaused =
         CVarDef.Create("server.start_server_paused", true, CVar.SERVERONLY);
+
+    /// <summary>
+    /// Moffstation - Automatically pauses the lobby (until an admin unpauses) when the server reaches 0 players
+    /// </summary>
+    public static readonly CVarDef<bool> EmptyAutoPause =
+        CVarDef.Create("server.empty_auto_pause", true, CVar.SERVERONLY);
 }
