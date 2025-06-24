443d2ddc490a0f02e3d8a83934240d47a20a4adb
mq <113324899+mqole@users.noreply.github.com>
2025-06-24 17:15:17 -0600

    thaven ion storm functionality (#2850)
    * ion storm parity
    
    * cowboy!
    
    * oops lol
    
    * random fix
    
    * tiny wee guidebook tweak
    
    * add/remove/replace
    
    * oops!

--- Content.Server/StationEvents/Events/IonStormRule.cs
+++ Content.Server/StationEvents/Events/IonStormRule.cs
@@ -7,11 +7,14 @@ using Content.Shared.Station.Components;
 using Content.Shared._Moffstation.Traits.Components;
 using Content.Shared._Moffstation.Traits.EntitySystems;
 //Moffstation - End
+using Robust.Shared.Random; // imp
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
 {
+    // [Dependency] private readonly IonStormSystem _ionStorm = default!; // imp remove
+    [Dependency] private readonly IRobustRandom _random = default!; // imp
     [Dependency] private readonly IonStormSystem _ionStorm = default!;
     [Dependency] private readonly SharedEmpVulnerableSystem _empVulnerable = default!; //Moffstation - EMP Vulnerability
 
@@ -22,14 +25,20 @@ public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
         if (!TryGetRandomStation(out var chosenStation))
             return;
 
-        var query = EntityQueryEnumerator<SiliconLawProviderComponent, TransformComponent, IonStormTargetComponent>();
-        while (query.MoveNext(out var ent, out var lawBound, out var xform, out var target))
+        // begin imp edit, why tf wasnt this all just an event
+        // var query = EntityQueryEnumerator<SiliconLawBoundComponent, TransformComponent, IonStormTargetComponent>();
+        var query = EntityQueryEnumerator<IonStormTargetComponent, TransformComponent>();
+        while (query.MoveNext(out var ent, out var target, out var xform))
+        // end imp edit
         {
-            // only affect law holders on the station
-            if (CompOrNull<StationMemberComponent>(xform.GridUid)?.Station != chosenStation)
+            // only affect law holders on the station, and check random chance (imp edit)
+            if (CompOrNull<StationMemberComponent>(xform.GridUid)?.Station != chosenStation ||
+                !_random.Prob(target.Chance)) // imp
                 continue;
-
-            _ionStorm.IonStormTarget((ent, lawBound, target));
+            // begin imp edit again
+            var ev = new IonStormEvent();
+            RaiseLocalEvent(ent, ref ev);
+            //     _ionStorm.IonStormTarget((ent, lawBound, target));
         }
 
         //Moffstation - Begin - EMP Vulnerability
@@ -45,3 +54,10 @@ public sealed class IonStormRule : StationEventSystem<IonStormRuleComponent>
         //Moffstation - End
     }
 }
+
+// imp add
+/// <summary>
+/// Event raised on an entity with <see cref="IonStormTargetComponent"/> when an ion storm occurs on the attached station.
+/// </summary>
+[ByRefEvent]
+public record struct IonStormEvent(bool Adminlog = true);
