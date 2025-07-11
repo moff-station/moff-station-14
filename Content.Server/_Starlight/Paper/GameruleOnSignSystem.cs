ed7d505494aca740b5cc4c87d1850ede75fbe6ff
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-07-11 14:30:55 -0600

    some fixes

--- Content.Server/_Starlight/Paper/GameruleOnSignSystem.cs
+++ Content.Server/_Starlight/Paper/GameruleOnSignSystem.cs
@@ -27,30 +27,30 @@ public sealed class GameruleOnSignSystem : EntitySystem
         SubscribeLocalEvent<GameruleOnSignComponent, PaperSignedEvent>(OnPaperSigned);
     }
 
-    private void OnPaperSigned(EntityUid uid, GameruleOnSignComponent component, PaperSignedEvent args)
+    private void OnPaperSigned(Entity<GameruleOnSignComponent> ent, ref PaperSignedEvent args)
     {
         // Check if they've already signed the paper, if not add them to the list
-        if (!component.SignedEntityUids.Add(args.Signer))
+        if (!ent.Comp.SignedEntityUids.Add(args.Signer))
             return;
 
-        if (!_whitelistSystem.CheckBoth(args.Signer, component.Blacklist, component.Whitelist))
+        if (!_whitelistSystem.CheckBoth(args.Signer, ent.Comp.Blacklist, ent.Comp.Whitelist))
             return;
 
-        if (component.SignaturesNeeded > 0)
+        if (ent.Comp.SignaturesNeeded > 0)
         {
-            component.SignaturesNeeded--;
+            ent.Comp.SignaturesNeeded--;
 
-            if (component.SignaturesNeeded <= 0 && _random.NextFloat() < component.GameruleChance)
+            if (ent.Comp.SignaturesNeeded <= 0 && _random.NextFloat() < ent.Comp.GameruleChance)
             {
-                foreach (var rule in component.Rules)
+                foreach (var rule in ent.Comp.Rules)
                 {
-                    var ent = _gameTicker.AddGameRule(rule.Id);
-                    _gameTicker.StartGameRule(ent);
+                    var ruleEnt = _gameTicker.AddGameRule(rule.Id);
+                    _gameTicker.StartGameRule(ruleEnt);
                 }
             }
         }
 
-        if (component.Charges > 0 && _random.NextFloat() < component.TriggerChance)
+        if (ent.Comp.Charges > 0 && _random.NextFloat() < ent.Comp.TriggerChance)
         {
             // If it can't do it's job right, then dont subtract from the charges
             if (!TryComp(args.Signer, out ActorComponent? actor))
@@ -59,22 +59,22 @@ public sealed class GameruleOnSignSystem : EntitySystem
             if (!_mind.TryGetMind(actor.PlayerSession.UserId, out var mind, out var mindComponent))
                 return;
 
-            component.Charges--;
+            ent.Comp.Charges--;
 
 
-            foreach (var antag in component.Antags)
+            foreach (var antag in ent.Comp.Antags)
             {
-                // var targetComp = _componentFactory.GetComponent(antag.TargetComponent);
+                // var targetComp = _ent.CompFactory.GetComponent(antag.TargetComponent);
                 // Evil function (though usage is probably correct). There isn't infrastructure around gamerules targeting specific people, so we'll just hit em with this.
                 _antag.ForceMakeAntag<AntagSelectionComponent>(actor.PlayerSession, antag);
             }
 
-            if (component.Objectives.Count > 0)
+            if (ent.Comp.Objectives.Count > 0)
             {
                 // Wipe all the current objectives so they can be overriden
                 while (_mind.TryRemoveObjective(mind.Value, mindComponent, 0));
 
-                foreach (var objective in component.Objectives)
+                foreach (var objective in ent.Comp.Objectives)
                 {
                     _mind.TryAddObjective(mind.Value, mindComponent, objective.Id);
                 }
