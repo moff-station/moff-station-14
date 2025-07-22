c12b8a8d2838c7bb56dc757c6024e54da9fbd498
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-07-21 23:07:57 -0600

    Adding some comments

--- Content.Server/_Moffstation/GameTicking/Rules/LoadGridRuleSystem.cs
+++ Content.Server/_Moffstation/GameTicking/Rules/LoadGridRuleSystem.cs
@@ -13,11 +13,11 @@ public sealed class LoadGridRuleSystem : GameRuleSystem<LoadGridRuleComponent>
     [Dependency] private readonly MapLoaderSystem _mapLoader = default!;
     [Dependency] private readonly StationSystem _stationSystem = default!;
 
-
     protected override void Added(EntityUid uid, LoadGridRuleComponent component, GameRuleComponent gameRule, GameRuleAddedEvent args)
     {
         base.Added(uid, component, gameRule, args);
 
+        //Get the station
         if (!TryGetRandomStation(out var station) ||
             !TryComp<StationDataComponent>(station, out var data) ||
             data.Grids.Count < 1)
@@ -27,10 +27,10 @@ public sealed class LoadGridRuleSystem : GameRuleSystem<LoadGridRuleComponent>
             return;
         }
 
+        // Get the map that the main station exists on
         if (_stationSystem.GetLargestGrid(data) is not { } largestGrid)
             return;
         var map = Transform(largestGrid).MapID;
-
         if (map == MapId.Nullspace)
         {
             Log.Warning($"Attempted to load grid into nullspace for rule {args.RuleId}!");
@@ -38,8 +38,10 @@ public sealed class LoadGridRuleSystem : GameRuleSystem<LoadGridRuleComponent>
             return;
         }
 
+        // Get the next offset of the grid
         var offset = RobustRandom.NextVector2(component.MinimumDistance, component.MaximumDistance);
 
+        // Load the grid
         if (!_mapLoader.TryLoadGrid(map, component.GridPath, out _, null, offset))
         {
             Log.Warning($"Unable to load grid for {args.RuleId}!");
