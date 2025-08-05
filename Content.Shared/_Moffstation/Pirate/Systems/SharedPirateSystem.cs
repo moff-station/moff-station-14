338b6b66262e37b8388c6da7f635973e51b59cee
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-08-05 09:09:25 -0600

    Fixes

--- Content.Shared/_Moffstation/Pirate/Systems/SharedPirateSystem.cs
+++ Content.Shared/_Moffstation/Pirate/Systems/SharedPirateSystem.cs
@@ -1,32 +1,3 @@
-using Content.Shared._Moffstation.Pirate.Components;
-using Content.Shared.Antag;
-using Robust.Shared.GameStates;
-using Robust.Shared.Player;
-
 namespace Content.Shared._Moffstation.Pirate.Systems;
 
-public abstract class SharedPirateSystem : EntitySystem
-{
-    public override void Initialize()
-    {
-        base.Initialize();
-
-        SubscribeLocalEvent<PirateComponent, ComponentGetStateAttemptEvent>(OnPirateAttemptGetState);
-    }
-
-    private void OnPirateAttemptGetState(
-        Entity<PirateComponent> entity,
-        ref ComponentGetStateAttemptEvent args)
-    {
-        args.Cancelled = !CanGetState(args.Player);
-    }
-
-    private bool CanGetState(ICommonSession? player)
-    {
-        //Apparently this can be null in replays so I am just returning true.
-        if (player?.AttachedEntity is not {} uid)
-            return true;
-
-        return HasComp<PirateComponent>(uid);
-    }
-}
+public abstract class SharedPirateSystem : EntitySystem;
