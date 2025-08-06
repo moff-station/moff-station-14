0e34a3b3f3372ee48fd69a63e3dfc415d5e4e1a9
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-08-06 11:58:43 -0600

    added guard comments

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -21,9 +21,11 @@ public sealed class CargoTest
     private static readonly HashSet<ProtoId<CargoProductPrototype>> Ignored =
     [
         // This is ignored because it is explicitly intended to be able to sell for more than it costs.
+        // Moffstation - Start - test fixes
         new("FunCrateGambling"),
         new("PirateSyndicateSurplusBundle"),
         new("PirateGrandLottery")
+        // Moffstation - End
     ];
 
     [Test]
