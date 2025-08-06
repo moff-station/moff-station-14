ab51faf817a7c042bfaf7dd75d4c24d76afc587c
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-08-06 11:57:00 -0600

    Added pirate gambling crates to ignore list

--- Content.IntegrationTests/Tests/CargoTest.cs
+++ Content.IntegrationTests/Tests/CargoTest.cs
@@ -21,7 +21,9 @@ public sealed class CargoTest
     private static readonly HashSet<ProtoId<CargoProductPrototype>> Ignored =
     [
         // This is ignored because it is explicitly intended to be able to sell for more than it costs.
-        new("FunCrateGambling")
+        new("FunCrateGambling"),
+        new("PirateSyndicateSurplusBundle"),
+        new("PirateGrandLottery")
     ];
 
     [Test]
