a8af41ef1586a82d3d5571ab0d62cc06ea67d67e
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-09-03 14:08:01 -0600

    Namespaces

--- Content.Server/Radio/EntitySystems/HeadsetSystem.cs
+++ Content.Server/Radio/EntitySystems/HeadsetSystem.cs
@@ -7,7 +7,7 @@ using Content.Shared.Radio.Components;
 using Content.Shared.Radio.EntitySystems;
 using Robust.Shared.Network;
 using Robust.Shared.Player;
-using Robust.Shared.Prototypes;
+using Robust.Shared.Prototypes; // Moffstation
 
 namespace Content.Server.Radio.EntitySystems;
 
@@ -15,7 +15,7 @@ public sealed class HeadsetSystem : SharedHeadsetSystem
 {
     [Dependency] private readonly INetManager _netMan = default!;
     [Dependency] private readonly RadioSystem _radio = default!;
-    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;   // Moffstation - Remove Common
 
 
     public override void Initialize()
