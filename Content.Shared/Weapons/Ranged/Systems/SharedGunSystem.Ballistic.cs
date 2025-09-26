e1aa7b90b9647d33143d59ce01dd283d3664193e
PanzerVo <harlee_94@yahoo.com>
2025-09-27 09:54:27 +1000

    Initial.

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Ballistic.cs
@@ -100,7 +100,7 @@ public abstract partial class SharedGunSystem
         // Continuous loading
         _doAfter.TryStartDoAfter(new DoAfterArgs(EntityManager, args.User, component.FillDelay, new AmmoFillDoAfterEvent(), used: uid, target: args.Target, eventTarget: uid)
         {
-            BreakOnMove = true,
+            BreakOnMove = false,
             BreakOnDamage = false,
             NeedHand = true,
         });
