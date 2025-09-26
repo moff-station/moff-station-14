f2e0903e085bc63fb5f7472f92a76552ae0d13b2
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-09-26 14:26:47 -0600

    adjustments

--- Content.Server/_Moffstation/Speech/LongSpeechComponent.cs
+++ Content.Server/_Moffstation/Speech/LongSpeechComponent.cs
@@ -5,17 +5,26 @@ namespace Content.Server._Moffstation.Speech;
 [RegisterComponent]
 public sealed partial class LongSpeechComponent : Component
 {
+    /// <summary>
+    /// How many sounds are left in the currect speech
+    /// </summary>
     [DataField]
     public int SyllablesLeft;
 
+    /// <summary>
+    /// The max amount of sounds that can be played in one speech
+    /// </summary>
     [DataField]
     public int MaxSyllables = 10;
 
     [DataField]
     public SoundSpecifier Sound;
 
+    /// <summary>
+    /// The pitch variation applied to the speech
+    /// </summary>
     [DataField]
-    public float PitchVariation = 0.2f;
+    public float PitchVariation = 0.05f;
 
     [DataField]
     public string Message;
@@ -23,8 +32,11 @@ public sealed partial class LongSpeechComponent : Component
     [DataField]
     public TimeSpan Cooldown = TimeSpan.FromSeconds(0.2f);
 
+    /// <summary>
+    /// The time variation in seconds on the cooldown
+    /// </summary>
     [DataField]
-    public float TimeVariation = 0.05f;
+    public float TimeVariation;
 
     [DataField]
     public TimeSpan NextSpeak;
