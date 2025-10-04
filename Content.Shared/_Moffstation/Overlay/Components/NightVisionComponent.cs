b6a2b515585da3182874008e080e2ebb2a171404
Centronias <me@centronias.com>
2025-10-04 13:15:59 -0700

    fixes #633

--- Content.Shared/_Moffstation/Overlay/Components/NightVisionComponent.cs
+++ Content.Shared/_Moffstation/Overlay/Components/NightVisionComponent.cs
@@ -1,10 +1,21 @@
 using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
 
 namespace Content.Shared._Moffstation.Overlay.Components;
 
-/// <summary>
-/// When this component is on a player-controlled entity, it applies a night vision visual effect and creates a client-
-/// only pointlight to illuminate the area around the entity.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class NightVisionComponent : Component;
+/// When this component is on a player-controlled entity, it applies a night vision visual effect
+/// (<see cref="ShaderPrototype"/>) and creates a <see cref="EffectPrototype">client-only effect entity</see> with a
+/// pointlight to illuminate the area around the entity.
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class NightVisionComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public EntProtoId EffectPrototype = "EffectNightVision";
+
+    [DataField, AutoNetworkedField]
+    public EntProtoId ShaderPrototype = "ModernNightVisionShader";
+
+    /// The instance of <see cref="EffectPrototype"/>..
+    [ViewVariables]
+    public EntityUid? Effect;
+}
