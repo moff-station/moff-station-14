78d1a25ef04fe043598413aa7d28ab303aa1e5d9
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-10-22 13:57:01 -0600

    Stamp other people

---
+++ Content.Shared/_Moffstation/Paper/Systems/StampSystem.cs
@@ -0,0 +1,35 @@
+using Content.Shared.Paper;
+using Content.Shared.Popups;
+using Content.Shared.Weapons.Melee.Events;
+using Robust.Shared.Player;
+
+namespace Content.Shared._Moffstation.Paper.Systems;
+
+public sealed class StampSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPopupSystem _popupSystem = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<StampComponent, MeleeHitEvent>(OnAttack);
+    }
+
+    private void OnAttack(Entity<StampComponent> ent, ref MeleeHitEvent args)
+    {
+        foreach (var hitEnt in args.HitEntities)
+        {
+            var stampPaperOtherMessage = Loc.GetString("paper-component-action-stamp-paper-other",
+                ("user", args.User),
+                ("target", hitEnt),
+                ("stamp", args.Weapon));
+
+            _popupSystem.PopupEntity(stampPaperOtherMessage, args.User, Filter.PvsExcept(args.User, entityManager: EntityManager), true);
+
+            var stampPaperSelfMessage = Loc.GetString("paper-component-action-stamp-paper-self",
+                ("target", hitEnt),
+                ("stamp", args.Weapon));
+            _popupSystem.PopupClient(stampPaperSelfMessage, args.User, args.User);
+        }
+    }
+}
