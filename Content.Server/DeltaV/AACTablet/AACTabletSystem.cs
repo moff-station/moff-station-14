7c4eb91947e13832cd1b3beafd085b9c5624c746
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-11-19 21:08:52 -0700

    lil fix

--- Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
+++ Content.Server/DeltaV/AACTablet/AACTabletSystem.cs
@@ -1,4 +1,5 @@
 using Content.Server.Chat.Systems;
+using Content.Shared.Chat;
 using Content.Shared.DeltaV.AACTablet;
 using Content.Shared.DeltaV.QuickPhrase;
 using Content.Shared.IdentityManagement;
