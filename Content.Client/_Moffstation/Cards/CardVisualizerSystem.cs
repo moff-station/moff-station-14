5472d380facfc04914fcae926d7ee63135690b9c
Centronias <me@centronias.com>
2025-11-25 12:42:12 -0800

    Cards utilize strip menu obscuring

--- Content.Client/_Moffstation/Cards/CardVisualizerSystem.cs
+++ Content.Client/_Moffstation/Cards/CardVisualizerSystem.cs
@@ -1,12 +1,24 @@
 using System.Linq;
 using Content.Client._Moffstation.GameObjects;
 using Content.Shared._Moffstation.Cards.Components;
+using Content.Shared._Moffstation.Strip.Components;
 using Robust.Client.GameObjects;
+using Robust.Shared.Map;
 
 namespace Content.Client._Moffstation.Cards;
 
 public sealed class CardVisualizerSystem : ManagedLayerVisualizerSystem<CardComponent>
 {
+    [Dependency] private readonly MetaDataSystem _meta = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CardComponent, GetHideInStripMenuEntityEvent>(CardOnGetHideInStripMenuEntity);
+    }
+
     protected override ref HashSet<string> SpriteLayersAdded(CardComponent component) =>
         ref component.SpriteLayersAdded;
 
@@ -21,5 +33,22 @@ public sealed class CardVisualizerSystem : ManagedLayerVisualizerSystem<CardComp
         {
             layerFactory($"{layerIndex}", layerData);
         }
+    }
+
+    private void CardOnGetHideInStripMenuEntity(Entity<CardComponent> entity, ref GetHideInStripMenuEntityEvent args)
+    {
+        var virtualCard = Spawn(null, args.SpawnAt);
+        var meta = MetaData(entity);
+        var virtualMeta = MetaData(virtualCard);
+        _meta.SetEntityName(virtualCard, meta.EntityName, virtualMeta, raiseEvents: false);
+        _meta.SetEntityDescription(virtualCard, meta.EntityDescription, virtualMeta);
+
+        var sprite = new Entity<SpriteComponent?>(virtualCard, AddComp<SpriteComponent>(virtualCard));
+        foreach (var layer in entity.Comp.ReverseSprite)
+        {
+            SpriteSystem.AddLayer(sprite, layer, index: null);
+        }
+
+        args.Entity = virtualCard;
     }
 }
