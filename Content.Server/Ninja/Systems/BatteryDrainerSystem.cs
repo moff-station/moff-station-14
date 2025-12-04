11fdef26d9365a12255e046397df30d2d4626b5a
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-12-03 17:41:08 -0700

    Fixed it

--- Content.Server/Ninja/Systems/BatteryDrainerSystem.cs
+++ Content.Server/Ninja/Systems/BatteryDrainerSystem.cs
@@ -37,7 +37,7 @@ public sealed class BatteryDrainerSystem : SharedBatteryDrainerSystem
     /// </summary>
     private void OnStartup(Entity<BatteryDrainerComponent> ent, ref ComponentStartup args)
     {
-        if (ent.Comp.BatteryUid == null && HasComp<BatteryComponent>(ent.Owner))
+        if (ent.Comp.BatteryUid == null && (HasComp<BatteryComponent>(ent.Owner) || HasComp<PredictedBatteryComponent>(ent.Owner)))
             ent.Comp.BatteryUid = ent.Owner;
     }
 
@@ -55,7 +55,7 @@ public sealed class BatteryDrainerSystem : SharedBatteryDrainerSystem
         // handles even if battery is full so you can actually see the poup
         args.Handled = true;
 
-        if (_battery.IsFull(battery))
+        if (!HasComp<PredictedBatteryComponent>(battery) && _battery.IsFull(battery)) // Moffstation - Hack to get the predicted component working for this, remove the first condition if a fix happens
         {
             _popup.PopupEntity(Loc.GetString("battery-drainer-full"), uid, uid, PopupType.Medium);
             return;
@@ -122,12 +122,6 @@ public sealed class BatteryDrainerSystem : SharedBatteryDrainerSystem
         Spawn("EffectSparks", Transform(target).Coordinates);
         _audio.PlayPvs(comp.SparkSound, target);
         _popup.PopupEntity(Loc.GetString("battery-drainer-success", ("battery", target)), uid, uid);
-
-        // IMP ADD- god this code is a mess. the bool return is only ever used to check if this should repeat
-        // we dont want that if we're draining the full thing so whatever
-        if (comp.FullDrain)
-            return false;
-        // IMP ADD END
 
         // repeat the doafter until battery is full
         return !_predictedBattery.IsFull((comp.BatteryUid.Value, battery));
