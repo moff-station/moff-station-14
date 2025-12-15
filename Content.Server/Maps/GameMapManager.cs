762663dd84aa0ff3bfb5cb681e0449ab3377daeb
Jona-K <63449674+Katzenminer@users.noreply.github.com>
2025-12-16 00:22:05 +0100

    Update Content.Server/Maps/GameMapManager.cs
    Makes sense
    
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>

--- Content.Server/Maps/GameMapManager.cs
+++ Content.Server/Maps/GameMapManager.cs
@@ -88,14 +88,9 @@ public sealed class GameMapManager : IGameMapManager
 
         var maps = AllVotableMaps().ToArray();
         _random.Shuffle(maps);
-        //Moffstation - Begin - Fixed the Integration test failing due to out Vote Rollover system
-        foreach (var map in maps)//ensures every map gets an Entry and is initzilized
-        {
-            _rollOverVotes[map] = 0;
-        }
-        //Moffstation - End
         foreach (var map in maps)
         {
+            _rollOverVotes[map] = 0; // Moffstation - Initialize rollover votes for all maps.
             if (_previousMaps.Count >= _mapQueueDepth)
                 break;
             _previousMaps.Enqueue(map.ID);
