8732a174bd94c4a0c706b7eaf5e962f1e3d63452
Jona <jona.07@gmx.de>
2025-12-17 23:21:57 +0100

    BWAH

---
+++ Content.Client/_Moffstation/Prayer/PrayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using Robust.Client.Graphics;
+using Content.Client.Administration.Managers;
+using Content.Shared._Moffstation.Prayers;
+
+namespace Content.Client._Moffstation.Prayer;
+
+public sealed class PrayerHandler : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeNetworkEvent<PrayerEvent>(Onprayer);
+    }
+    [Dependency] private readonly IClyde _clyde = default!;
+    [Dependency] private readonly IClientAdminManager _adminManager = default!;
+
+    private void Onprayer(PrayerEvent args)
+    {
+        //check if player is an Admin && not deadming
+        var admin = _adminManager.GetAdminData();
+        if (admin != null)
+        {
+
+            _clyde.RequestWindowAttention();
+        }
+    }
+}
---
+++ Content.Shared/_Moffstation/Prayers/PrayerEvent.cs
@@ -0,0 +1,7 @@
+﻿using Robust.Shared.Serialization;
+
+namespace Content.Shared._Moffstation.Prayers;
+
+[Serializable, NetSerializable]
+public sealed partial class PrayerEvent : EntityEventArgs;
+
