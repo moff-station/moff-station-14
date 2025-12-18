7361bb1e7c569d033e08d08e0ad48ff10eca1a99
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-12-18 12:24:54 -0700

    Move to DummyRule.cs

--- Content.Server/_Moffstation/StationEvents/Components/DummyRuleComponent.cs
+++
@@ -1,6 +0,0 @@
-using Content.Server._Moffstation.StationEvents.Events;
-
-namespace Content.Server._Moffstation.StationEvents.Components;
-
-[RegisterComponent, Access(typeof(DummyRuleSystem))]
-public sealed partial class DummyRuleComponent : Component;
--- Content.Server/_Moffstation/StationEvents/Events/DummyRuleSystem.cs
+++
@@ -1,6 +0,0 @@
-using Content.Server._Moffstation.StationEvents.Components;
-using Content.Server.StationEvents.Events;
-
-namespace Content.Server._Moffstation.StationEvents.Events;
-
-public sealed class DummyRuleSystem : StationEventSystem<DummyRuleComponent>;
