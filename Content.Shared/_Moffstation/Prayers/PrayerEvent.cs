2d5d7bb98e7d2ed9602cbc1873ce613b0fe635e8
Jona-K <63449674+Katzenminer@users.noreply.github.com>
2025-12-18 01:32:21 +0100

    Update Content.Shared/_Moffstation/Prayers/PrayerEvent.cs
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: Jona-K <63449674+Katzenminer@users.noreply.github.com>

--- Content.Shared/_Moffstation/Prayers/PrayerEvent.cs
+++ Content.Shared/_Moffstation/Prayers/PrayerEvent.cs
@@ -2,6 +2,7 @@
 
 namespace Content.Shared._Moffstation.Prayers;
 
+/// This even is raised for Admin's clients to indicate that a prayer has been sent.
 [Serializable, NetSerializable]
 public sealed partial class PrayerEvent : EntityEventArgs;
 
