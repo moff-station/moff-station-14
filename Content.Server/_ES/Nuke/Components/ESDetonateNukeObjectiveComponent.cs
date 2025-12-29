2231a99696e90d976671131d40534f6900d8cd36
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-12-28 22:15:36 -0700

    Revert "Port ES objective system"
    This reverts commit 6a023c3104d7bbd420223c2054eda4f50437969d.

--- Content.Server/_ES/Nuke/Components/ESDetonateNukeObjectiveComponent.cs
+++
@@ -1,8 +0,0 @@
-namespace Content.Server._ES.Nuke.Components;
-
-/// <summary>
-/// Objective to detonate a nuke to win.
-/// </summary>
-[RegisterComponent]
-[Access(typeof(ESDetonateNukeObjectiveSystem))]
-public sealed partial class ESDetonateNukeObjectiveComponent : Component;
