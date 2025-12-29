f9911de862597582e4601a45c73882363e55e77e
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2025-12-28 22:15:36 -0700

    Revert "Port ES objective system"
    This reverts commit 6a023c3104d7bbd420223c2054eda4f50437969d.

--- Content.Shared/_ES/Objectives/Kill/Components/ESKillTargetObjectiveComponent.cs
+++
@@ -1,7 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared._ES.Objectives.Kill.Components;
-
-[RegisterComponent, NetworkedComponent]
-[Access(typeof(ESKillTargetObjectiveSystem))]
-public sealed partial class ESKillTargetObjectiveComponent : Component;
