5a55b08e1742300ec1be9c650f18afff77245182
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-01-15 22:18:53 -0700

    Antag Weights

---
+++ Content.Server/_Moffstation/Antag/Commands/AntagWeights.cs
@@ -0,0 +1,40 @@
+using System.Linq;
+using Content.Server.Administration;
+using Content.Shared.Administration;
+using Robust.Server.Player;
+using Robust.Shared.Console;
+
+namespace Content.Server._Moffstation.Antag.Commands;
+
+[AdminCommand(AdminFlags.Debug)]
+public sealed class AntagWeights : LocalizedEntityCommands
+{
+    [Dependency] private readonly IPlayerManager _players = default!;
+    [Dependency] private readonly WeightedAntagManager _antagWeight = default!;
+
+    public override string Command => "antagweights";
+    public override string Description => "Gets the current antag weights";
+    public override string Help => "antagweights";
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        var users = _players.Sessions
+            .OrderBy(c => _antagWeight.GetWeight(c.UserId))
+            .ToArray();
+
+        var total = users.Sum(user => _antagWeight.GetWeight(user.UserId));
+
+        foreach (var user in users)
+        {
+            shell.WriteLine(Loc.GetString("cmd-antagweight-list",
+                ("player", user.Name),
+                ("weight", _antagWeight.GetWeight(user.UserId)),
+                ("percent", _antagWeight.GetWeight(user.UserId) / total * 100)));
+        }
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        return CompletionResult.FromHint(Loc.GetString("cmd-antagweight-completion"));
+    }
+}
