bf71011b48de09a1aa6c5911da2ea0b60f86fede
Jona <jona.07@gmx.de>
2026-01-17 05:45:12 +0100

    You say nothin *smoke bomb*

--- Content.Server/_Moffstation/GameTicking/Commands/SetCountdownCommand.cs
+++ Content.Server/_Moffstation/GameTicking/Commands/SetCountdownCommand.cs
@@ -6,28 +6,28 @@ using Robust.Shared.Console;
 namespace Content.Server._Moffstation.GameTicking.Commands;
 
 [AdminCommand(AdminFlags.Round)]
-public sealed class SetCountdownCommand : LocalizedCommands
+public sealed class SetCountdownCommand : LocalizedEntityCommands
 {
+    [Dependency] private readonly GameTicker _gameTicker = default!;
+
     public override string Command => "setcountdown";
 
     public override void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        var gameTicker = IoCManager.Resolve<GameTicker>();
-
-        if (gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
+        if (_gameTicker.RunLevel != GameRunLevel.PreRoundLobby)
         {
             shell.WriteLine(Loc.GetString("shell-can-only-run-from-pre-round-lobby"));
             return;
         }
 
-        if (args.Length < 1 || !uint.TryParse(args[0], out var seconds) || seconds == 0)
+        if (!uint.TryParse(args[0], out var seconds) || seconds == 0)
         {
-            shell.WriteLine(Loc.GetString("cmd-setcountdown-invalid-seconds", ("value", args.Length > 0 ? args[0] : "none")));
+            shell.WriteLine(Loc.GetString("cmd-setcountdown-invalid-seconds", ("value", args[0])));
             return;
         }
 
         var time = TimeSpan.FromSeconds(seconds);
-        if (!gameTicker.SetCountdown(time))
+        if (!_gameTicker.SetCountdown(time))
         {
             shell.WriteLine(Loc.GetString("cmd-setcountdown-too-late"));
         }
