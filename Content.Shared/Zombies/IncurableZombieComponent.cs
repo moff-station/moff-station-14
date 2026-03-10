7ece30d7238036fc17e84537e4adb9b76ab711df
Nyxilath <colton.malone@gmail.com>
2026-03-10 16:12:39 -0400

    forgor moffstation tag

--- Content.Shared/Zombies/IncurableZombieComponent.cs
+++ Content.Shared/Zombies/IncurableZombieComponent.cs
@@ -1,5 +1,5 @@
 ﻿using Robust.Shared.Prototypes;
-using Content.Shared.Chemistry.Components;
+using Content.Shared.Chemistry.Components; // Moffstation - Infectious blood
 
 namespace Content.Shared.Zombies;
 
