5d412793b75569f03ecdc460c5444b1ed57c05c2
Nyxilath <colton.malone@gmail.com>
2026-03-15 15:35:56 -0400

    init: dedicated component

---
+++ Content.Shared/_Moffstation/Traits/Components/FeebleComponent.cs
@@ -0,0 +1,9 @@
+﻿using Content.Shared.Weapons.Melee;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Moffstation.Traits.Components;
+
+// Like to the DisarmProne Component, but used as a trait rather than a punishment.
+[RegisterComponent, NetworkedComponent]
+[Access(typeof(SharedMeleeWeaponSystem))]
+public sealed partial class FeebleComponent : Component;
