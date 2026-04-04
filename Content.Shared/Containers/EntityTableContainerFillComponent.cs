3862007ebc3249ccfbd1858a01404c03d762c5c0
Centronias <me@centronias.com>
2026-04-04 12:25:44 -0700

    document my crimes

--- Content.Shared/Containers/EntityTableContainerFillComponent.cs
+++ Content.Shared/Containers/EntityTableContainerFillComponent.cs
@@ -13,6 +13,8 @@ public sealed partial class EntityTableContainerFillComponent : Component
 
     // Moffstation - Begin - Allow EntityTableContainerFillComponent to fail gracefully
     /// If false, trying to overfill a container fill will emit an error.
+    /// Ideally this'd be a flag per container in <see cref="Containers"/>, but that is a much larger blast radius that
+    /// I don't want to deal with when making a kinda hackfix to upstream code.
     [DataField]
     public bool SkipFillsWhichDontFit = false;
     // Moffstation - End
