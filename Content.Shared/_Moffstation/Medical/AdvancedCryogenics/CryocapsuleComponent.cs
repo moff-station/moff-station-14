fa969bf39da7faa0647d9521118b8685f9d7ee08
StudMuffin <casalebalthazar@gmail.com>
2026-04-23 04:04:26 +0200

    added some ambiance and started component logic (now can contain brain)

---
+++ Content.Shared/_Moffstation/Medical/AdvancedCryogenics/CryocapsuleComponent.cs
@@ -0,0 +1,22 @@
+using Content.Shared.Containers.ItemSlots;
+
+namespace Content.Shared._Moffstation.Medical.AdvancedCryogenics;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class CryocapsuleComponent : Component
+{
+    /// <summary>
+    /// The ID of the itemslot that holds the brain.
+    /// </summary>
+    [DataField]
+    public string BrainSlotId = "brain_slot";
+
+    /// <summary>
+    /// The <see cref="ItemSlot"/> for this capsule. Holds the brain.
+    /// </summary>
+    [DataField(required: true)]
+    public ItemSlot BrainSlot = new();
+}
---
+++ Content.Shared/_Moffstation/Medical/AdvancedCryogenics/CryocapsuleSystem.cs
@@ -0,0 +1,24 @@
+using Content.Shared.Containers.ItemSlots;
+
+namespace Content.Shared._Moffstation.Medical.AdvancedCryogenics;
+
+/// <summary>
+/// This handles...
+/// </summary>
+public sealed class CryocapsuleSystem : EntitySystem
+{
+    /// <inheritdoc/>
+
+    [Dependency] private readonly ItemSlotsSystem _itemSlots = default!;
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<CryocapsuleComponent, ComponentInit>(OnCryocapsuleInit);
+    }
+
+    private void OnCryocapsuleInit(Entity<CryocapsuleComponent> ent, ref ComponentInit args)
+    {
+        _itemSlots.AddItemSlot(ent.Owner, ent.Comp.BrainSlotId, ent.Comp.BrainSlot);
+    }
+}
