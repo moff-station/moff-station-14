560e71e00f7aaa3cf73d77bb68f5279077571682
Centronias <me@centronias.com>
2026-04-24 12:29:58 -0700

    apparently you can network them

--- Content.Shared/_Moffstation/Silicons/Borgs/ActionBorgModuleComponent.cs
+++ Content.Shared/_Moffstation/Silicons/Borgs/ActionBorgModuleComponent.cs
@@ -19,6 +19,6 @@ public sealed partial class ActionBorgModuleComponent : Component
     [DataField(required: true), AutoNetworkedField]
     public List<EntProtoId> Actions = new();
 
-    [DataField]
+    [DataField, AutoNetworkedField]
     public List<EntityUid> ActionEntities = new();
 }
