147f5ec83344f13712ed08fc1cd5a009eea9b258
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-05-13 00:17:16 -0600

    opening shift

--- Content.Server/_CD/Spawners/ArrivalsSpawnPointSystem.cs
+++ Content.Server/_CD/Spawners/ArrivalsSpawnPointSystem.cs
@@ -1,6 +1,9 @@
 using System.Linq;
+using Content.Server.Chat.Managers;
 using Content.Server.GameTicking;
-using Content.Shared.CCVar;
+using Content.Server.GameTicking.Events;
+using Content.Shared._Moffstation.CCVar;
+using Content.Shared.Chat;
 using Content.Shared.GameTicking;
 using Content.Shared.Preferences;
 using Content.Shared.Roles;
@@ -20,22 +23,48 @@ public sealed class ArrivalsSpawnPointSystem : EntitySystem
     [Dependency] private readonly IGameTiming _gameTiming = default!;
     [Dependency] private readonly IConfigurationManager _cfgManager = default!;
     [Dependency] private readonly GameTicker _gameTicker = default!;
+    [Dependency] private readonly IChatManager _chat = default!;
 
+    // The chance that there will be player who spawn on station at all
+    // as in, if this rolls people will spawn on station, and if it doesn't, nobody will spawn on station
+    private const float NormalSpawnChance = 0.6f;
+    // If people are to spawn normally, how many player max should spawn on station?
+    private const int NormalSpawnMaxLimit = 5;
+
+    private int _normalSpawnLimit;
+    private int _normalSpawnCount;
 
     public override void Initialize()
     {
+        SubscribeLocalEvent<RoundStartingEvent>(OnRoundStart);
         SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawn);
+    }
+
+    private void OnRoundStart(RoundStartingEvent args)
+    {
+        _normalSpawnCount = 0;
+        _normalSpawnLimit = _random.Prob(NormalSpawnChance) ? _random.Next(1, NormalSpawnMaxLimit) : 0;
     }
 
     private void OnPlayerSpawn(PlayerSpawnCompleteEvent args)
     {
         // Check if cvar disables this feature
-        if (!_cfgManager.GetCVar(CCVars.StartAtArrivals))
+        if (!_cfgManager.GetCVar(MoffCCVars.StartAtArrivals))
             return;
+
+        // This should be random... unless people are spawned in a certain order? who knows
+        if (_normalSpawnCount < _normalSpawnLimit)
+        {
+            _normalSpawnCount++;
+            var message = Loc.GetString("opening-shift-greeting");
+            var wrappedMessage = Loc.GetString("chat-manager-server-wrap-message", ("message", message));
+            _chat.ChatMessageToOne(ChatChannel.Server, message, wrappedMessage, default, false, args.Player.Channel, Color.CornflowerBlue);
+            return;
+        }
 
         // If it's a latejoin and past the forced arrivals timer, allow choosing cryosleep
         if (args is { LateJoin: true, Profile.SpawnPriority: not SpawnPriorityPreference.Arrivals } &&
-            _gameTiming.CurTime.Subtract(_gameTicker.RoundStartTimeSpan) > TimeSpan.FromMinutes(_cfgManager.GetCVar(CCVars.SpawnPreferenceDelay)))
+            _gameTiming.CurTime.Subtract(_gameTicker.RoundStartTimeSpan) > TimeSpan.FromMinutes(_cfgManager.GetCVar(MoffCCVars.SpawnPreferenceDelay)))
             return;
 
         // Ensure they have a job, so that we won't end up making mobs spawn on arrivals.
