55efb7efddd45c74524fa155c064e6523e84e142
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-05-18 13:13:04 -0600

    unused random

--- Content.Shared/_ES/Camera/SharedESScreenshakeSystem.cs
+++ Content.Shared/_ES/Camera/SharedESScreenshakeSystem.cs
@@ -15,7 +15,6 @@ namespace Content.Shared._ES.Camera;
 // DeltaV - renamed from ESScreenshakeSystem to SharedESScreenshakeSystem
 public sealed class SharedESScreenshakeSystem : EntitySystem
 {
-    [Dependency] private readonly IRobustRandom _random = default!;
     [Dependency] private readonly IGameTiming _timing = default!;
 
     #region Internal
