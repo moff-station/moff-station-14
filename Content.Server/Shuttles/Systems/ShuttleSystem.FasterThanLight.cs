d210ab058afb9a7b892b6f28105249d00e0026d8
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-05-21 00:42:31 -0600

    Port Beestation FTL noises

--- Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
+++ Content.Server/Shuttles/Systems/ShuttleSystem.FasterThanLight.cs
@@ -34,14 +34,14 @@ public sealed partial class ShuttleSystem
      * This is a way to move a shuttle from one location to another, via an intermediate map for fanciness.
      */
 
-    private readonly SoundSpecifier _startupSound = new SoundPathSpecifier("/Audio/Effects/Shuttle/hyperspace_begin.ogg")
+    private readonly SoundSpecifier _startupSound = new SoundPathSpecifier("/Audio/_Moffstation/Effects/Shuttle/hyperspace_begin.ogg") // Moffstation - Custom FTL noises
     {
-        Params = AudioParams.Default.WithVolume(-5f),
+        Params = AudioParams.Default.WithVolume(-2f), // Moffstation
     };
 
-    private readonly SoundSpecifier _arrivalSound = new SoundPathSpecifier("/Audio/Effects/Shuttle/hyperspace_end.ogg")
+    private readonly SoundSpecifier _arrivalSound = new SoundPathSpecifier("/Audio/_Moffstation/Effects/Shuttle/hyperspace_end.ogg") // Moffstation - custom FTL noises
     {
-        Params = AudioParams.Default.WithVolume(-5f),
+        Params = AudioParams.Default.WithVolume(-2f), // Moffstation
     };
 
     public float DefaultStartupTime;
