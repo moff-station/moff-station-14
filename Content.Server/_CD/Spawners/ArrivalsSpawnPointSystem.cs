3c1d746a8bdca1c56fd43d852314e644ca3faef5
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-05-22 13:58:36 -0600

    actually nvm

--- Content.Server/_CD/Spawners/ArrivalsSpawnPointSystem.cs
+++ Content.Server/_CD/Spawners/ArrivalsSpawnPointSystem.cs
@@ -38,14 +38,7 @@ public sealed partial class ArrivalsSpawnPointSystem : EntitySystem
     {
         var manager = Spawn();
         var comp = AddComp<ArrivalsSpawnManagerComponent>(manager);
-        if (_random.Prob(comp.StationSpawnChance))
-        {
-            comp.StationSpawnLimit = _random.Next(1, comp.StationSpawnMaxLimit);
-        }
-        else
-        {
-            QueueDel(manager);
-        }
+        comp.StationSpawnLimit = _random.Prob(comp.StationSpawnChance) ? _random.Next(1, comp.StationSpawnMaxLimit) : 0;
     }
 
     private void OnPlayerSpawn(PlayerSpawnCompleteEvent args)
