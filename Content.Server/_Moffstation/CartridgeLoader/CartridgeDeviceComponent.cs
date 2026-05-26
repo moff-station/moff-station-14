6663982f79aa139288658beb4bdfb6947bce7081
StudMuffin <casalebalthazar@gmail.com>
2026-05-26 22:45:49 +0200

    added some tools

---
+++ Content.Server/_Moffstation/CartridgeLoader/CartridgeDeviceComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Server._Moffstation.CartridgeLoader;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class CartridgeDeviceComponent : Component
+{
+    
+}
