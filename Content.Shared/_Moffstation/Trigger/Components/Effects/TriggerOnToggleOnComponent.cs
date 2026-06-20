a443f6f26ef89fe21290452bd957fcebd563f219
Centronias <me@centronias.com>
2026-06-19 20:04:18 -0700

    fancy rebocrobo stuff

---
+++ Content.Shared/_Moffstation/Trigger/Components/Effects/TriggerOnToggleOnComponent.cs
@@ -0,0 +1,18 @@
+﻿using Content.Shared.Trigger.Components.Triggers;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Moffstation.Trigger.Components.Effects;
+
+/// <summary>
+/// This component fires <see cref="BaseTriggerOnXComponent.KeyOut"/> when its entity is toggled on.
+/// The user is the toggling event's user.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class TriggerOnToggleOnComponent : BaseTriggerOnXComponent;
+
+/// <summary>
+/// This component fires <see cref="BaseTriggerOnXComponent.KeyOut"/> when its entity is toggled off.
+/// The user is the toggling event's user.
+/// </summary>
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+public sealed partial class TriggerOnToggleOffComponent : BaseTriggerOnXComponent;
