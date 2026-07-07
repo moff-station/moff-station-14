082dcaa448b238108bda8d19ed81b0e48370f6e4
Nyxilath <colton.malone@gmail.com>
2026-07-06 21:48:16 -0400

    no this isn't stolen pirate code, you can't steal from pirates

--- Content.Server/_Moffstation/GameTicking/Rules/ListeningOutpostRuleSystem.cs
+++ Content.Server/_Moffstation/GameTicking/Rules/ListeningOutpostRuleSystem.cs
@@ -2,13 +2,24 @@ using Content.Server._Moffstation.GameTicking.Rules.Components;
 using Content.Server.Antag;
 using Content.Server.GameTicking;
 using Content.Server.GameTicking.Rules;
+using Content.Server.Station.Systems;
 using Content.Shared.GameTicking.Components;
+using Content.Shared._Moffstation.ListeningOutpost.Components;
+using Content.Shared.Cargo.Components;
 
 namespace Content.Server._Moffstation.GameTicking.Rules;
 
 public sealed partial class ListeningOutpostRuleSystem : GameRuleSystem<ListeningOutpostRuleComponent>
 {
     [Dependency] private AntagSelectionSystem _antag = default!;
+    [Dependency] private StationSystem _station = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ListeningOutpostRuleComponent, RuleLoadedGridsEvent>(OnRuleLoadedGrids);
+    }
 
     protected override void AppendRoundEndText(EntityUid uid,
         ListeningOutpostRuleComponent listeningOutpostRuleComponent,
@@ -23,6 +34,30 @@ public sealed partial class ListeningOutpostRuleSystem : GameRuleSystem<Listenin
         foreach (var (_, sessionData, name) in antags)
         {
             args.AddLine(Loc.GetString("lpo-list-name-user", ("name", name), ("user", sessionData.UserName)));
+        }
+    }
+
+    private void OnRuleLoadedGrids(Entity<ListeningOutpostRuleComponent> ent, ref RuleLoadedGridsEvent args)
+    {
+        // Check each added grid to see if it's a listening outpost base.
+        foreach (var grid in args.Grids)
+        {
+            if (!TryComp<ListeningOutpostBaseComponent>(grid, out var baseComp))
+                continue;
+
+            baseComp.AssociatedRule = ent;
+
+            // Converts the listening outpost base into a station, giving it a functional cargo system
+            ent.Comp.AssociatedStation = _station.InitializeNewStation(ent.Comp.StationConfig, [grid]);
+
+            // Give the listening outpost station component a reference to this rule for later reference
+            if (!TryComp<ListeningOutpostStationComponent>(ent.Comp.AssociatedStation, out var stationComp))
+                continue;
+            stationComp.AssociatedRule = GetNetEntity(ent.Owner);
+
+            // Turns the listening outpost base into a trade station, so that its buy/sell pads are functional
+            //EnsureComp<TradeStationComponent>(grid);
+            //Dirty(grid, baseComp);
         }
     }
 }
---
+++ Content.Shared/_Moffstation/ListeningOutpost/Components/ListeningOutpostBaseComponent.cs
@@ -0,0 +1,28 @@
+﻿using Content.Shared.Cargo.Prototypes;
+using JetBrains.Annotations;
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Moffstation.ListeningOutpost.Components;
+
+/// <summary>
+/// Tags a grid as the listening outpost base, which causes it to become station-like and capable of using listening outpost trade functionality.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+public sealed partial class ListeningOutpostBaseComponent : Component
+{
+    [DataField]
+    [AutoNetworkedField]
+    public EntityUid AssociatedRule;
+}
+
+/// <summary>
+/// Tags an entity as a listening outpost station, which may be made of many grids.
+/// </summary>
+[RegisterComponent, NetworkedComponent, UsedImplicitly]
+public sealed partial class ListeningOutpostStationComponent : Component
+{
+    [DataField]
+    public NetEntity? AssociatedRule;
+}
