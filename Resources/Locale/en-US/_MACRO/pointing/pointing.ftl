ultramario1998/imp-station-14

pointing modifiers (#3337)
* sending to stony 2 test

* oop

* crosshair

* OOPS OOPS OOPS

* lol

* new crosshair

* numbah 3

* locaels

* broken localhost = 9 zillion commits

* comments & yml

* req changes

* resolve merge conflicts

* goes on all our guns

* yup

Resources/Locale/en-US/_Impstation/pointing/pointing.ftl
@@ -1,0 +1,20 @@
+## PointingSystem copy
+
+imp-pointing-system-try-point-cannot-reach = You can't reach there!
+imp-pointing-system-point-at-self = You {$verb} at yourself.
+imp-pointing-system-point-at-other = You {$verb} at {THE($other)}.
+imp-pointing-system-point-at-self-others = {CAPITALIZE(THE($otherName))} {$verb} at imp-{REFLEXIVE($other)}.
+imp-pointing-system-point-at-other-others = {CAPITALIZE(THE($otherName))} {$verb} at {THE($other)}.
+imp-pointing-system-point-at-you-other = {CAPITALIZE(THE($otherName))} {$verb} at you.
+imp-pointing-system-point-at-tile = You {$verb} at the {$tileName}.
+imp-pointing-system-point-in-own-inventory-self = You {$verb} at your {$item}.
+imp-pointing-system-point-in-own-inventory-others = {CAPITALIZE(THE($pointer))} {$verb} at {THE($pointer)}'s {$item}.
+imp-pointing-system-point-in-other-inventory-self = You {$verb} at {THE($wearer)}'s {$item}.
+imp-pointing-system-point-in-other-inventory-target = {CAPITALIZE(THE($pointer))} {$verb} at your {$item}.
+imp-pointing-system-point-in-other-inventory-others = {CAPITALIZE(THE($pointer))} {$verb} at {THE($wearer)}'s {$item}.
+imp-pointing-system-other-point-at-tile = {CAPITALIZE(THE($otherName))} {$verb} at the {$tileName}.
+
+pointing-system-verb-point-self = point
+pointing-system-verb-point-other = points
+pointing-system-verb-take-aim-self = take aim
+pointing-system-verb-take-aim-other = takes aim

moff-station/moff-station-14

Resources/Locale/en-US/_MACRO/pointing/pointing.ftl
@@ -1,0 +1,20 @@
+## PointingSystem copy
+
+macro-pointing-system-try-point-cannot-reach = You can't reach there!
+macro-pointing-system-point-at-self = You {$verb} at yourself.
+macro-pointing-system-point-at-other = You {$verb} at {THE($other)}.
+macro-pointing-system-point-at-self-others = {CAPITALIZE(THE($otherName))} {$verb} at macro-{REFLEXIVE($other)}.
+macro-pointing-system-point-at-other-others = {CAPITALIZE(THE($otherName))} {$verb} at {THE($other)}.
+macro-pointing-system-point-at-you-other = {CAPITALIZE(THE($otherName))} {$verb} at you.
+macro-pointing-system-point-at-tile = You {$verb} at the {$tileName}.
+macro-pointing-system-point-in-own-inventory-self = You {$verb} at your {$item}.
+macro-pointing-system-point-in-own-inventory-others = {CAPITALIZE(THE($pointer))} {$verb} at {THE($pointer)}'s {$item}.
+macro-pointing-system-point-in-other-inventory-self = You {$verb} at {THE($wearer)}'s {$item}.
+macro-pointing-system-point-in-other-inventory-target = {CAPITALIZE(THE($pointer))} {$verb} at your {$item}.
+macro-pointing-system-point-in-other-inventory-others = {CAPITALIZE(THE($pointer))} {$verb} at {THE($wearer)}'s {$item}.
+macro-pointing-system-other-point-at-tile = {CAPITALIZE(THE($otherName))} {$verb} at the {$tileName}.
+
+pointing-system-verb-point-self = point
+pointing-system-verb-point-other = points
+pointing-system-verb-take-aim-self = take aim
+pointing-system-verb-take-aim-other = takes aim