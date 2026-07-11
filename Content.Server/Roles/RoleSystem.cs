e673cd848acd39b7bb616acc55d09a77d031001e
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-07-11 07:15:34 -0600

    New character menu UI (#1605)
    * Fancy character menu UI... YIPPEEE
    
    * Oh yeah we got shit twerking
    
    * okay I should probably save
    
    * oh wow
    
    * some adjustments
    
    * typo fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Signed-off-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    
    * Updates
    
    * poop
    
    * AAAAAAAAAAAAA
    
    * Apply suggestions from code review
    
    Co-authored-by: Nyxilath <colton.malone@gmail.com>
    Signed-off-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    
    * Gorlexbob
    
    ---------
    
    Signed-off-by: DuckManZach <144298822+DuckManZach@users.noreply.github.com>
    Co-authored-by: Centronias <charlie.t.santos@gmail.com>
    Co-authored-by: Nyxilath <colton.malone@gmail.com>

--- Content.Server/Roles/RoleSystem.cs
+++ Content.Server/Roles/RoleSystem.cs
@@ -25,21 +25,19 @@ public sealed partial class RoleSystem : SharedRoleSystem
             return null;
         }
 
-        var ev = new GetBriefingEvent();
-
-        // This is on the event because while this Entity<T> is also present on every Mind Role Entity's MindRoleComp
-        // getting to there from a GetBriefing event subscription can be somewhat boilerplate
-        // and this needs to be looked up for the event anyway so why calculate it again later
-        ev.Mind = (mindId.Value, mindComp);
-
-        // Briefing is no longer raised on the mind entity itself
-        // because all the components that briefings subscribe to should be on Mind Role Entities
+        // Moffstation - Start - only show the latest mind role's briefing
+        string? latestBriefing = null;
         foreach (var role in mindComp.MindRoleContainer.ContainedEntities)
         {
+            var ev = new GetBriefingEvent();
+            ev.Mind = (mindId.Value, mindComp);
             RaiseLocalEvent(role, ref ev);
+            if (ev.Briefing != null)
+                latestBriefing = ev.Briefing;
         }
 
-        return ev.Briefing;
+        return latestBriefing;
+        // Moffstation - End
     }
 
     public void RoleUpdateMessage(MindComponent mind)
@@ -93,15 +91,19 @@ public sealed class GetBriefingEvent
     /// If there is no briefing, sets it to the string.
     /// If there is a briefing, adds a new line to separate it from the appended string.
     /// </summary>
+    // Moff - We are making this wipe it out BECAUSE we ONLY WANT ONE!!!
+    // Uh, This is probably not a good way to do it.
+    // BUT! If we don't want to change every instance of this being used now and in the future then this is the way to go.
     public void Append(string text)
     {
-        if (Briefing == null)
-        {
-            Briefing = text;
-        }
-        else
-        {
-            Briefing += "\n" + text;
-        }
+        Briefing = text;
+        // if (Briefing == null)
+        // {
+        //     Briefing = text;
+        // }
+        // else
+        // {
+        //     Briefing += "\n" + text;
+        // }
     }
 }
