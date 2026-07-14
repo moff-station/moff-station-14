f803ab9f2d2c3b8db123313aa3cb7f5a58803620
Nyxilath <colton.malone@gmail.com>
2026-07-14 14:48:36 -0400

    BEHOLD! MY SHITCODE!

---
+++ Content.Server/_Moffstation/Hellportal/Components/HellportalComponent.cs
@@ -0,0 +1,24 @@
+﻿using Content.Shared.EntityTable.EntitySelectors;
+using Robust.Shared.Audio;
+
+namespace Content.Server._Moffstation.Hellportal.Components;
+
+[RegisterComponent]
+public sealed partial class HellportalComponent : Component
+{
+    [DataField(required: true)]
+    public EntityTableSelector BasicSpawnTable;
+
+    [DataField]
+    public float Accumulator = 0f;
+
+    [DataField]
+    public float SpawnCooldown = 30f;
+
+    [DataField]
+    public int MaxSpawns = 100;
+
+    [DataField]
+    public SoundSpecifier? Sound;
+
+}
---
+++ Content.Server/_Moffstation/Hellportal/HellportalSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using Content.Server._Moffstation.Hellportal.Components;
+using Content.Shared.EntityTable;
+using Robust.Shared.Audio.Systems;
+
+namespace Content.Server._Moffstation.Hellportal;
+
+public sealed partial class HellportalSystem : EntitySystem
+{
+    [Dependency] private EntityTableSystem _entityTable = default!;
+    [Dependency] private SharedAudioSystem _audio = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent < HellportalComponent, AnchorStateChangedEvent>(OnAnchorChange);
+
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<HellportalComponent, TransformComponent>();
+        var totalCount = EntityQuery<HellportalMobComponent>().Count();
+
+        while (query.MoveNext(out var uid, out var comp, out var xform))
+        {
+            comp.Accumulator += frameTime;
+            var spawns = _entityTable.GetSpawns(comp.BasicSpawnTable);
+
+            if (comp.Accumulator > comp.SpawnCooldown)
+            {
+                comp.Accumulator -= comp.SpawnCooldown;
+
+                if (totalCount < comp.MaxSpawns)
+                {
+                    _audio.PlayPvs(comp.Sound, xform.Coordinates);
+                    foreach (var proto in spawns)
+                    {
+                        Spawn(proto, xform.Coordinates);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnAnchorChange(EntityUid uid, HellportalComponent component, ref AnchorStateChangedEvent args)
+    {
+        if (!args.Anchored)
+        {
+            QueueDel(uid);
+        }
+    }
+}
