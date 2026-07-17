3a4f9ec1b424547cb512dd64c2f2857f6b869d11
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-07-17 13:09:12 -0600

    boom truke

--- Content.Server/_Moffstation/Antag/AntagSelectionSystem.Weights.cs
+++ Content.Server/_Moffstation/Antag/AntagSelectionSystem.Weights.cs
@@ -3,14 +3,16 @@ using System.Linq;
 using Content.Server._Moffstation.Antag;
 using Content.Server.GameTicking;
 using Content.Shared.GameTicking;
+using Content.Shared.Mind;
 using Content.Shared.Roles;
+using Robust.Shared.Network;
 using Robust.Shared.Prototypes;
 
 // ReSharper disable once CheckNamespace
 namespace Content.Server.Antag;
 
 /// <summary>
-/// Moff - Players who dont' get selected to be antag get an extra name in the hat for future rounds
+/// Moff - Players who don't get selected to be antag get an extra name in the hat for future rounds
 /// This continues stacking until they roll antag, to which they will get reset to a weight of 1
 /// </summary>
 public sealed partial class AntagSelectionSystem
@@ -32,37 +34,47 @@ public sealed partial class AntagSelectionSystem
         if (args.New != GameRunLevel.PostRound)
             return;
 
-        // Walk the weighted (non-exempt) antag rules that ran this round. Gather the antag preferences they handed out,
-        // and consume the weight of anyone who actually rolled one of them.
-        var distributed = new List<ProtoId<AntagPrototype>>();
+        var antagRoleList = new List<ProtoId<AntagPrototype>>();
+        var antagUsers = new HashSet<NetUserId>();
         var rules = QueryAllRules();
         while (rules.MoveNext(out var uid, out var comp, out _))
         {
-            // Rules marked MoffUnweightedAntag opt out of weighting entirely - their antags neither accrue nor consume.
+            // Check if the rule is exempt from changing weights
             if (HasComp<MoffUnweightedAntagComponent>(uid))
                 continue;
 
             foreach (var antag in comp.Antags)
             {
                 if (ProtoMan.Resolve(antag.Proto, out var def))
-                    distributed.AddRange(def.PrefRoles);
+                    antagRoleList.AddRange(def.PrefRoles);
             }
 
-            // Rolled a weighted antag, so their accrued weight is spent - back down to 1.
-            foreach (var session in comp.PreSelectedSessions.Values.SelectMany(sessions => sessions))
+            // Get the UserIds of the players who were antags due to the rules in question
+            foreach (var (mindId, _) in comp.AssignedMinds.Values.SelectMany(minds => minds))
             {
-                if (_role.PlayerIsAntagonist(session))
-                    _weightedAntagMan.SetWeight(session.UserId, 1);
+                if (TryComp<MindComponent>(mindId, out var mind) && mind.UserId is { } userId)
+                    antagUsers.Add(userId);
             }
         }
 
-        // Everyone who didn't roll an antag but opted into one that actually ran gets another entry in the hat.
+        // If they were found in one of the rules, back down to 1
+        foreach (var userId in antagUsers)
+        {
+            _weightedAntagMan.SetWeight(userId, 1);
+        }
+
+        // Look through all the other players
         foreach (var session in _playerManager.Sessions)
         {
-            if (_role.PlayerIsAntagonist(session))
+            // Check if they're actually in round.
+            if (!GameTicker.UserHasJoinedGame(session))
                 continue;
 
-            if (TryGetValidAntagPreferences(session, distributed))
+            if (antagUsers.Contains(session.UserId))
+                continue;
+
+            // Only increment if their preferences match
+            if (TryGetValidAntagPreferences(session, antagRoleList))
                 _weightedAntagMan.IncrementWeight(session.UserId);
         }
     }
--- Content.Server/_Moffstation/Antag/MoffUnweightedAntagComponent.cs
+++ Content.Server/_Moffstation/Antag/MoffUnweightedAntagComponent.cs
@@ -1,8 +1,10 @@
 namespace Content.Server._Moffstation.Antag;
 
 /// <summary>
-/// This is used for marking that an antag shouldn't influence weighted selection
-/// In other words, if you don't get selected for this role, you don't get an antag weight
+/// If you don't get selected for a rule with this component, you don't get an antag weight.
+/// However, because of how antag selection is set up right now, your weight still influences your odds of being selected.
+/// Having it do otherwise would be very complex and I don't feel like setting it up right now! its also fairly inconsequential atm
 /// </summary>
+// TODO: Make rules with this component not use weights in selection
 [RegisterComponent]
 public sealed partial class MoffUnweightedAntagComponent : Component;
