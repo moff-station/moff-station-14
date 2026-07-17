4657e6e5c5a4b6cd15f3f9d3e4f3777a7d2db514
StudMuffin <casalebalthazar@gmail.com>
2026-07-17 20:59:28 +0200

    Apply suggestions from code review
    Co-authored-by: StudMuffin <casalebalthazar@gmail.com>
    Signed-off-by: StudMuffin <casalebalthazar@gmail.com>

--- Content.Server/_Moffstation/StationEvents/Components/IonAttackRuleComponent.cs
+++ Content.Server/_Moffstation/StationEvents/Components/IonAttackRuleComponent.cs
@@ -18,18 +18,18 @@ public sealed partial class IonAttackRuleComponent : Component
     /// <summary>
     /// Chance to remove a random law from the starting lawset
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public float RemoveChance = 0.2f;
 
     /// <summary>
     /// Chance to replace a random law from the starting lawset with a new one
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField)]
     public float ReplaceChance = 0.2f;
 
     /// <summary>
     /// Chance to shuffle the laws of the starting lawset
     /// </summary>
-    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
     public float ShuffleChance = 0.2f;
 }
