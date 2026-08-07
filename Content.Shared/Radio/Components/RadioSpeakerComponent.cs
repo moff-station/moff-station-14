be31af834becc286d34b42aa965913fad7d632df
Huaqas <zacswebb@gmail.com>
2026-08-07 11:27:55 +0800

    Another Comment

--- Content.Shared/Radio/Components/RadioSpeakerComponent.cs
+++ Content.Shared/Radio/Components/RadioSpeakerComponent.cs
@@ -2,7 +2,7 @@ using Content.Shared.Radio.EntitySystems;
 using Content.Shared.Chat;
 using Robust.Shared.GameStates;
 using Robust.Shared.Prototypes;
-using Content.Shared._Goob.StationRadio.Systems; // Moffstation
+using Content.Shared._Goobstation.StationRadio.Systems; // Moffstation - _Goob -> _Goobstation
 
 namespace Content.Shared.Radio.Components;
 
