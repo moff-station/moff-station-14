0517a9ff50fb029ddc65f23a95961264f44e6502
phönix <pacifist.jules@gmail.com>
2026-08-10 10:59:08 +0300

    Predict intercom examine. (#43107)
    * radiosystem is funny
    
    * review
    
    Co-authored-by: slarticodefast <slarticodefast@users.noreply.github.com>
    Co-authored-by: moonheart08 <moonheart08@users.noreply.github.com>
    
    * dont forget your tests
    
    * dont forget your loc
    
    * networking
    
    Co-authored-by: Tayrtahn <Tayrtahn@users.noreply.github.com>
    
    * dont return because its breaks
    
    * uh
    
    * review
    
    Co-authored-by: Pok245 <113675512+Pok27@users.noreply.github.com>
    
    * more predictions and cleanup
    
    * refactor: just me, being picky about whitespaces and commetns formatting
    
    ---------
    
    Co-authored-by: slarticodefast <slarticodefast@users.noreply.github.com>
    Co-authored-by: moonheart08 <moonheart08@users.noreply.github.com>
    Co-authored-by: Tayrtahn <Tayrtahn@users.noreply.github.com>
    Co-authored-by: Tayrtahn <tayrtahn@gmail.com>
    Co-authored-by: Pok245 <113675512+Pok27@users.noreply.github.com>
    Co-authored-by: Fildrance <fildrance@gmail.com>

--- Content.Shared/Radio/EntitySystems/SharedRadioSystem.cs
+++ Content.Shared/Radio/EntitySystems/SharedRadioSystem.cs
@@ -1,62 +1,41 @@
-using Content.Shared.Radio.Components;
+using JetBrains.Annotations;
 using Robust.Shared.Prototypes;
 
 namespace Content.Shared.Radio.EntitySystems;
 
 /// <summary>
-///     This system handles intrinsic radios and the general process of converting radio messages into chat messages.
+/// This system handles intrinsic radios and the general process of converting radio messages into chat messages.
 /// </summary>
-public abstract class SharedRadioSystem : EntitySystem
+public abstract partial class SharedRadioSystem : EntitySystem
 {
     /// <summary>
-    /// Adds a single <see cref="RadioChannelPrototype"/> to an entity's <see cref="IntrinsicRadioTransmitterComponent"/>.
+    /// Send radio message to all active radio listeners.
     /// </summary>
-    /// <param name="ent">The entity to add the channel for.</param>
-    /// <param name="channel">The channel to add.</param>
-    /// <returns>True if added successfully, otherwise False.</returns>
-    public bool AddIntrinsicTransmitterChannel(Entity<IntrinsicRadioTransmitterComponent?> ent, ProtoId<RadioChannelPrototype> channel)
+    [PublicAPI]
+    public void SendRadioMessage(EntityUid messageSource,
+        string message,
+        ProtoId<RadioChannelPrototype> channel,
+        EntityUid radioSource,
+        bool escapeMarkup = true)
     {
-        if (!Resolve(ent.Owner, ref ent.Comp, false))
-            return false;
-
-        var success = ent.Comp.Channels.Add(channel);
-        Dirty(ent);
-
-        return success;
+        SendRadioMessage(messageSource, message, ProtoMan.Index(channel), radioSource, escapeMarkup: escapeMarkup);
     }
 
     /// <summary>
-    /// Removes <see cref="RadioChannelPrototype"/> from an entity's <see cref="IntrinsicRadioTransmitterComponent"/>.
+    /// Sends a radio message to all active radio listeners.
     /// </summary>
-    /// <param name="ent">The entity to remove the channel from.</param>
-    /// <param name="channel">The channel to remove.</param>
-    /// <returns>True if removed successfuly, otherwise False.</returns>
-    public bool RemoveIntrinsicTransmitterChannel(Entity<IntrinsicRadioTransmitterComponent?> ent, ProtoId<RadioChannelPrototype> channel)
+    /// <param name="messageSource">Entity that spoke the message.</param>
+    /// <param name="message">Message to send over the radio.</param>
+    /// <param name="channel">Radio channel to send the message on.</param>
+    /// <param name="radioSource">Entity transmitting the message.</param>
+    /// <param name="escapeMarkup">Whether markup in the message should be escaped.</param>
+    [PublicAPI]
+    public virtual void SendRadioMessage(EntityUid messageSource,
+        string message,
+        RadioChannelPrototype channel,
+        EntityUid radioSource,
+        bool escapeMarkup = true)
     {
-        if (!Resolve(ent.Owner, ref ent.Comp, false))
-            return false;
 
-        var success = ent.Comp.Channels.Remove(channel);
-        Dirty(ent);
-
-        return success;
-    }
-
-    /// <summary>
-    /// Sets the channel list in a <see cref="IntrinsicRadioTransmitterComponent"/> to an existing Hashset.
-    /// Used when you don't wanna have to add or remove every channel one by one.
-    /// </summary>
-    /// <param name="ent">The entity to set the channels for.</param>
-    /// <param name="channels">Hashset of the channels to set.</param>
-    /// <returns>True if set successfully, otherwise False.</returns>
-    public bool SetIntrinsicTransmitterChannels(Entity<IntrinsicRadioTransmitterComponent?> ent, HashSet<ProtoId<RadioChannelPrototype>> channels)
-    {
-        if (!Resolve(ent.Owner, ref ent.Comp, false))
-            return false;
-
-        ent.Comp.Channels = channels;
-        Dirty(ent);
-
-        return true;
     }
 }
