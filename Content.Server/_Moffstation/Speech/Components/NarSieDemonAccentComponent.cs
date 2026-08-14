2ae45376ea7cacb280531920b847f89ac419152e
Nyxilath <colton.malone@gmail.com>
2026-08-13 21:38:42 -0400

    OOP forgot that

--- Content.Server/_Moffstation/Speech/Components/NarSieDemonAccentComponent.cs
+++ Content.Server/_Moffstation/Speech/Components/NarSieDemonAccentComponent.cs
@@ -1,7 +1,4 @@
-﻿namespace Content.Server._Moffstation.Speech.Components
-{
-    [RegisterComponent]
-    public sealed partial class NarSieDemonAccentComponent : Component
-    {
-    }
-}
+﻿namespace Content.Server._Moffstation.Speech.Components;
+
+[RegisterComponent]
+public sealed partial class NarSieDemonAccentComponent : Component;
