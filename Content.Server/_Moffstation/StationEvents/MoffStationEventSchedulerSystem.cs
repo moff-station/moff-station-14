96165c406ec529c4a7951780e0c2ce2db3335124
DuckManZach <144298822+DuckManZach@users.noreply.github.com>
2026-08-27 17:05:05 -0600

    pb

--- Content.Server/_Moffstation/StationEvents/MoffStationEventSchedulerSystem.cs
+++ Content.Server/_Moffstation/StationEvents/MoffStationEventSchedulerSystem.cs
@@ -10,7 +10,6 @@ namespace Content.Server._Moffstation.StationEvents;
 public sealed partial class MoffStationEventSchedulerSystem : GameRuleSystem<MoffStationEventSchedulerComponent>
 {
     [Dependency] private EventManagerSystem _event = default!;
-    [Dependency] private IGameTiming _timing = default!;
 
     protected override void Started(
         EntityUid uid,
@@ -27,7 +26,8 @@ public sealed partial class MoffStationEventSchedulerSystem : GameRuleSystem<Mof
         }
 
         EnterState(component, component.InitialState, state);
-        component.NextEventTime = _timing.CurTime + TimeSpan.FromSeconds(component.InitialDelay.Next(RobustRandom));
+        // Overrides the first event's time
+        component.NextEventTime = Timing.CurTime + TimeSpan.FromSeconds(component.InitialDelay.Next(RobustRandom));
     }
 
     protected override void Ended(
@@ -52,10 +52,13 @@ public sealed partial class MoffStationEventSchedulerSystem : GameRuleSystem<Mof
 
         foreach (var ent in EntityQueryEnumerator<MoffStationEventSchedulerComponent, GameRuleComponent>())
         {
+            if (!GameTicker.IsGameRuleActive(ent))
+                continue;
+
             if (ent.Comp1.CurrentState is not { } stateId || !ent.Comp1.States.TryGetValue(stateId, out var state))
                 continue;
 
-            if (ent.Comp1.NextStateTime <= _timing.CurTime)
+            if (ent.Comp1.NextStateTime <= Timing.CurTime)
             {
                 if (state.EventOnEnd)
                     _event.RunRandomEvent(ent.Comp1.ScheduledGameRules);
@@ -67,10 +70,10 @@ public sealed partial class MoffStationEventSchedulerSystem : GameRuleSystem<Mof
             if (state.MinMaxEventTiming is not { } eventTiming)
                 continue;
 
-            if (ent.Comp1.NextEventTime > _timing.CurTime)
+            if (ent.Comp1.NextEventTime > Timing.CurTime)
                 continue;
 
-            ent.Comp1.NextEventTime = _timing.CurTime + TimeSpan.FromSeconds(eventTiming.Next(RobustRandom));
+            ent.Comp1.NextEventTime = Timing.CurTime + TimeSpan.FromSeconds(eventTiming.Next(RobustRandom));
             _event.RunRandomEvent(ent.Comp1.ScheduledGameRules);
         }
     }
@@ -119,7 +122,7 @@ public sealed partial class MoffStationEventSchedulerSystem : GameRuleSystem<Mof
 
         // These are absolute times, not durations, since Update compares them against CurTime.
         var (duration, eventDelay) = RollState(state);
-        component.NextStateTime = duration is { } stateDuration ? _timing.CurTime + stateDuration : null;
-        component.NextEventTime = _timing.CurTime + eventDelay;
+        component.NextStateTime = duration is { } stateDuration ? Timing.CurTime + stateDuration : null;
+        component.NextEventTime = Timing.CurTime + eventDelay;
     }
 }
