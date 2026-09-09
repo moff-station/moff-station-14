84a89e1501066559b914b36500431a1915288022
Wintoli <60529806+Wintoli@users.noreply.github.com>
2026-09-09 01:59:02 -0400

    Revert "Update Content.Shared/_Moffstation/JestographicSequencer/JestographicSequencerSystem.cs"
    This reverts commit 7ef1cfd65b47c3f5edb14d3e75a3bfe94acaf606.

--- Content.Shared/_Moffstation/JestographicSequencer/JestographicSequencerSystem.cs
+++ Content.Shared/_Moffstation/JestographicSequencer/JestographicSequencerSystem.cs
@@ -27,7 +27,13 @@ public sealed class JestographicSequencerSystem : EntitySystem
     [Dependency] private readonly SharedChargesSystem _charges = default!;
     [Dependency] private readonly SharedPopupSystem _popup = default!;
 
-    [LocalEventSubscription]
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<JestographicSequencerComponent, AfterInteractEvent>(OnAfterInteract);
+    }
+
     private void OnAfterInteract(Entity<JestographicSequencerComponent> ent, ref AfterInteractEvent args)
     {
         if (args.Handled || !args.CanReach || args.Target is not { } target)
