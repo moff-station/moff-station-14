d6b1dcab15baa373b24e89a78122be94f1204fe2
Doc <159411967+DrWhoFan13@users.noreply.github.com>
2026-09-18 21:48:43 -0700

    "Remove" faces from OwOAccentSystem.cs per maint request

--- Content.Shared/Speech/EntitySystems/OwOAccentSystem.cs
+++ Content.Shared/Speech/EntitySystems/OwOAccentSystem.cs
@@ -11,10 +11,11 @@ public sealed partial class OwOAccentSystem : RelayAccentSystem<OwOAccentCompone
     [Dependency] private IGameTiming _timing = default!;
     [Dependency] private IRobustRandom _random = default!;
 
+    /* Moff -  disable oWo accent faces? I guess?
     private static readonly IReadOnlyList<string> Faces =
     [
         " (•`ω´•)", " ;;w;;", " owo", " UwU", " >w<", " ^w^",
-    ];
+    ];*/
 
     private static readonly FrozenDictionary<string, string> SpecialWords =
         new Dictionary<string, string>
