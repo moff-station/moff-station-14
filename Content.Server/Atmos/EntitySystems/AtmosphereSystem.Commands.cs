9336929860c2e740f18ad801484754ccefaf1027
Mixelz <james@entropyfails.com>
2026-09-20 07:04:15 -0500

    Hot Air, Piping, Wiring
    Still need to finish up pipes/wires but its getting there.
    Also special Hot Air marker for Lava tiles @ 60c.

--- Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
+++ Content.Server/Atmos/EntitySystems/AtmosphereSystem.Commands.cs
@@ -69,7 +69,7 @@ public sealed partial class AtmosphereSystem
     /// <remarks>Please be responsible with this method. Used only by tests and fixgridatmos.</remarks>
     public void RebuildGridAtmosphere(Entity<GridAtmosphereComponent, MapGridComponent> ent)
     {
-        var mixtures = new GasMixture[9];
+        var mixtures = new GasMixture[10];
         for (var i = 0; i < mixtures.Length; i++)
         {
             mixtures[i] = new GasMixture(Atmospherics.CellVolume) { Temperature = Atmospherics.T20C };
@@ -106,6 +106,11 @@ public sealed partial class AtmosphereSystem
         // 8: Air (GM)
         mixtures[8].AdjustMoles(Gas.Oxygen, Atmospherics.OxygenMolesGasMiner);
         mixtures[8].AdjustMoles(Gas.Nitrogen, Atmospherics.NitrogenMolesGasMiner);
+
+        // 9: Hot Air @ 60C (GM)
+        mixtures[9].AdjustMoles(Gas.Oxygen, Atmospherics.OxygenMolesStandard);
+        mixtures[9].AdjustMoles(Gas.Nitrogen, Atmospherics.NitrogenMolesStandard);
+        mixtures[9].Temperature = 333.15f;
 
 
         // Force Invalidate & update air on all tiles
