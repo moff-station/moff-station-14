6c4a98a89921fe199ad6f4c416fb559373172c65
Doc <159411967+DrWhoFan13@users.noreply.github.com>
2026-09-22 19:31:01 -0700

    Implement Centronias's suggestions from Discord, and that's it for the night

--- Content.Shared/_Impstation/CrewMedal/SharedCrewMedalSystem.cs
+++ Content.Shared/_Impstation/CrewMedal/SharedCrewMedalSystem.cs
@@ -2,22 +2,21 @@ using Content.Shared.Examine;
 
 namespace Content.Shared._Impstation.CrewMedal;
 
-public abstract class SharedCrewMedalSystem : EntitySystem
+public partial abstract class SharedCrewMedalSystem : EntitySystem
 {
-    [SubscribeLocalEvent]
+    public override void Initialize()
     {
         base.Initialize();
         SubscribeLocalEvent<CrewMedalComponent, ExaminedEvent>(OnExamined);
     }
 
-    [SubscribeLocalEvent]
     private void OnExamined(Entity<CrewMedalComponent> medal, ref ExaminedEvent args)
     {
         if (!medal.Comp.Awarded)
             return;
 
         // Harmony Change Start - UI Formatting Change
-        localAwardString = medal.Comp.Reason == String.Empty ? "comp-crew-medal-inspection-text" : "comp-crew-medal-inspection-text-with-reason";
+        var localAwardString = medal.Comp.Reason == String.Empty ? "comp-crew-medal-inspection-text" : "comp-crew-medal-inspection-text-with-reason";
         var str = Loc.GetString(localAwardString, ("recipient", medal.Comp.Recipient), ("reason", medal.Comp.Reason));
         // Harmony Change End
         args.PushMarkup(str);
