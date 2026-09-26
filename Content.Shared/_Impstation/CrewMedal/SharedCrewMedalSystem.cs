6d8a354845869ddad08db1c84cbb889f4cceb3da
Doc <159411967+DrWhoFan13@users.noreply.github.com>
2026-09-25 17:32:22 -0700

    The shot in the dark didn't fix things

--- Content.Shared/_Impstation/CrewMedal/SharedCrewMedalSystem.cs
+++ Content.Shared/_Impstation/CrewMedal/SharedCrewMedalSystem.cs
@@ -2,7 +2,7 @@ using Content.Shared.Examine;
 
 namespace Content.Shared._Impstation.CrewMedal;
 
-public partial class SharedCrewMedalSystem : EntitySystem
+public partial abstract class SharedCrewMedalSystem : EntitySystem
 {
     public override void Initialize()
     {
